75da4f32559e389793ee39fbc62ac08a50b463cc
Valentino Amato <valentinoamatoroberts@gmail.com>
2024-02-29 18:03:04 -0300

    Update main.rs
    Define module data_handling.
    Create SaveData instance.

--- save_editor/src/main.rs
+++ save_editor/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use crate::data_handling::save::SaveData;
+use std::error::Error;
+
+mod data_handling;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    
+    let data = SaveData::build("testsave", "Proyectito")?;
+    println!("Health: {}",data.player.health);
+
+    Ok(())
 }
