d25d912ed632995b77f252c5c611deedb57a3633
Valentino Amato <valentinoamatoroberts@gmail.com>
2024-03-01 09:45:34 -0300

    Create new module 'enums'.
    Enums used in data_handling will be defined there.

---
+++ save_editor/src/data_handling/enums.rs
@@ -0,0 +1,18 @@
+use std::{fmt, io};
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(io::Error),
+    CustomError(&'static str),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::IoError(err) => write!(f, "I/0 error: {}",err),
+            Error::CustomError(err) => write!(f, "Save error: {}",err),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
--- save_editor/src/data_handling/mod.rs
+++ save_editor/src/data_handling/mod.rs
@@ -1,2 +1,4 @@
 pub mod save;
 pub mod player;
+pub mod file;
+pub mod enums;
