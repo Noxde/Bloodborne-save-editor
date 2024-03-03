23a115e2763634b9ef2f3bbf5d577e8013f396e2
Mateo Tesorieri <70549780+Noxde@users.noreply.github.com>
2024-03-03 11:44:13 -0300

    PlayerData now has a Vector of Stat insted of multiple fields It uses serde_json to read the offsets.json and get each stat info

--- save_editor/src/data_handling/player.rs
+++ save_editor/src/data_handling/player.rs
@@ -1,52 +1,33 @@
+use serde::Deserialize;
+
 use super::file::FileData;
+use std::fs::File;
+use std::io::{self, BufReader};
+
+#[derive(Deserialize, Debug)]
+pub struct Stat {
+    pub name: String,
+    pub rel_offset: isize,
+    pub lenght: usize,
+    pub times: usize,
+    pub value: u32,
+}
 
 pub struct PlayerData {
-    pub health: u32,
-    pub stamina: u32,
-
-    pub echoes: u32,
-    pub insight: u32,
-
-    pub level: u32,
-    pub vitality: u32,
-    pub endurance: u32,
-    pub strength: u32,
-    pub skill: u32,
-    pub bloodtinge: u32,
-    pub arcane: u32,
+    pub stats: Vec<Stat>,
 }
 
 impl PlayerData {
-    pub fn new(file: &FileData) -> PlayerData {
-        // HP, Stamina
-        let health = file.get_number(-147, 4);
-        let stamina = file.get_number(-119, 4);
-        // Currencies
-        let echoes = file.get_number(-19, 4);
-        let insight = file.get_number(-35, 4);
-        // Stats
-        let level = file.get_number(-23, 4);
-        let vitality = file.get_number(-103, 4);
-        let endurance = file.get_number(-95, 4);
-        let strength = file.get_number(-79, 4);
-        let skill = file.get_number(-71, 4);
-        let bloodtinge = file.get_number(-63, 4);
-        let arcane = file.get_number(-55, 4);
+    pub fn new(file: &FileData) -> Result<PlayerData, io::Error> {
+        let json_file = File::open("offsets.json")?;
+        let reader = BufReader::new(json_file);
 
-        PlayerData {
-            health,
-            stamina,
-
-            echoes,
-            insight,
-
-            level,
-            vitality,
-            endurance,
-            strength,
-            skill,
-            bloodtinge,
-            arcane,
+        // Read the JSON contents of the file as Vec<Stat>.
+        let mut stats: Vec<Stat> = serde_json::from_reader(reader)?;
+        for s in &mut stats {
+            s.value = file.get_number(s.rel_offset, s.lenght);
         }
+
+        Ok(PlayerData { stats })
     }
 }
