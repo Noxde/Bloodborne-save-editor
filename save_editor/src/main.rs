c365bdc5494aed77c43ccd611a800deaed08ef9d
Valentino Amato <valentinoamatoroberts@gmail.com>
2024-03-03 11:09:21 -0300

    Define 'gui' and 'gui_main' modules

--- save_editor/src/main.rs
+++ save_editor/src/main.rs
@@ -2,6 +2,7 @@ use crate::data_handling::save::SaveData;
 use std::error::Error;
 
 mod data_handling;
+mod gui;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let data = SaveData::build("testsave", "Proyectito")?;
