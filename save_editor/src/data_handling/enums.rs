75422993e2033ee19a43e921c4763d14386e79bf
Valentino Amato <valentinoamatoroberts@gmail.com>
2024-03-03 11:02:07 -0300

    Add 'UiError' variant to 'Error' enum

--- save_editor/src/data_handling/enums.rs
+++ save_editor/src/data_handling/enums.rs
@@ -1,9 +1,11 @@
 use std::{fmt, io};
+use fltk::prelude::FltkError;
 
 #[derive(Debug)]
 pub enum Error {
     IoError(io::Error),
     CustomError(&'static str),
+    UiError(FltkError),
 }
 
 impl fmt::Display for Error {
@@ -11,6 +13,7 @@ impl fmt::Display for Error {
         match self {
             Error::IoError(err) => write!(f, "I/0 error: {}",err),
             Error::CustomError(err) => write!(f, "Save error: {}",err),
+            Error::UiError(err) => write!(f, "UI error: {}",err),
         }
     }
 }
