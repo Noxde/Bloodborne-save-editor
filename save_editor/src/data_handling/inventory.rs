64787c467c00b76b84cdd0dc73520d2a523f6e39
Mateo Tesorieri <70549780+Noxde@users.noreply.github.com>
2024-03-10 19:10:27 -0300

    Added methods for the inventory struct edit_item: Changes the quantity of an existing item using its slot index (in the file bytes and the item struct) add_item: Adds a new item to the inventory and updates the inventory

--- save_editor/src/data_handling/inventory.rs
+++ save_editor/src/data_handling/inventory.rs
@@ -25,7 +25,54 @@ pub struct Inventory {
     pub items: Vec<Item>,
 }
 
-pub fn build(bytes: &[u8]) -> Inventory {
+impl Inventory {
+    pub fn _edit_item(&mut self, bytes: &mut Vec<u8>, index: u8, value: u32) {
+        let value_endian = u32::to_le_bytes(value);
+        let (start, _) = inventory_offset(bytes);
+
+        for i in (start..bytes.len()).step_by(16) {
+            if index == bytes[i] {
+                for (i, b) in bytes[i + 12..i + 16].iter_mut().enumerate() {
+                    if let Some(item) = self.items.iter_mut().find(|item| item.index == index) {
+                        item.amount = value;
+                    }
+                    *b = value_endian[i];
+                }
+                break;
+            }
+        }
+    }
+
+    pub fn _add_item(&mut self, bytes: &mut Vec<u8>, id: u32, quantity: u32) {
+        let (_, inventory_end) = inventory_offset(bytes);
+        let endian_id = u32::to_le_bytes(id);
+        let endian_quantity = u32::to_le_bytes(quantity);
+
+        for i in 0..12 {
+            if i < 8 {
+                bytes[inventory_end + i] = endian_id[i % 4];
+            } else {
+                bytes[inventory_end + i] = endian_quantity[i % 4];
+            }
+        }
+        bytes[inventory_end + 3] = 0xB0;
+        bytes[inventory_end + 7] = 0x40;
+        bytes[inventory_end + 12] = bytes[inventory_end - 4] + 1;
+
+        let new_item = Item {
+            index: bytes[inventory_end + 12],
+            id: u32::from_le_bytes(endian_id),
+            first_part: 213,
+            second_part: 1233,
+            info: get_info(id).unwrap(),
+            amount: quantity,
+        };
+
+        self.items.push(new_item);
+    }
+}
+
+pub fn build(bytes: &Vec<u8>) -> Inventory {
     Inventory {
         items: parse_items(bytes),
     }
