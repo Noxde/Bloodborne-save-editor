51d6f55852b5515fb3ec39d5c525eae8236b007a
Noxde <mateo.tesorieri@hotmail.com>
2024-08-07 15:58:33 -0300

    Added tauri command to make a save

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,13 +2,23 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
-
 mod data_handling;
+
+use data_handling::save::SaveData;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![make_save])
         .run(tauri::generate_context!())?;
 
     Ok(())
+}
+
+#[tauri::command]
+fn make_save(path: &str, name: &str) -> Option<SaveData> {
+    match SaveData::build(path, name) {
+        Ok(s) => Some(s),
+        Err(_) => None
+    }
 }
