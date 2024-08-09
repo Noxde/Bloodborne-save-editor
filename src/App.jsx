dc4fcc3f8634ac7c864e481904bb385291805529
Noxde <mateo.tesorieri@hotmail.com>
2024-08-09 10:51:55 -0300

    Removed unused imports, replaced html with inv component Added useContext as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import { invoke, dialog, fs } from "@tauri-apps/api";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useContext } from "react";
 import Item from "./components/Item";
 import Nav from "./components/Nav";
 import SideBar from "./components/SideBar";
+import Inventory from "./components/Inventory";
+import { SaveContext } from "./context/context";
 
 function App() {
   const [save, setSave] = useState(null);
@@ -19,24 +20,13 @@ function App() {
   return (
     <div className="App">
       <Nav setLoading={setLoading} save={save} setSave={setSave} />
-      <main id="content">
+      <main>
         <SideBar />
-        {/* <p style={{ cursor: "pointer" }} onClick={readFile}>
-        Open
-        </p> */}
+        <SaveContext.Provider value={{ save, setSave }}>
+          {loading ? <div>Loading</div> : null}
 
-        {loading ? <div>Loading</div> : null}
-
-        {save != null ? (
-          <div style={{ overflowY: "scroll" }}>
-            <div
-              style={{ width: "795px", height: "90px", background: "grey" }}
-            ></div>
-            {save.inventory.articles.map((x) => (
-              <Item key={x.index} item={x} />
-            ))}
-          </div>
-        ) : null}
+          {save != null ? <Inventory /> : null}
+        </SaveContext.Provider>
       </main>
     </div>
   );
---
+++ src/context/context.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const SaveContext = createContext(null);
