a68ca24307072dd4924be7faaaa96a5d92ebc725
Noxde <mateo.tesorieri@hotmail.com>
2024-08-09 10:54:30 -0300

    Save button working now

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { invoke, dialog, fs } from "@tauri-apps/api";
 import { basename } from "@tauri-apps/api/path";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 function Nav({ setLoading, setSave, save }) {
   const [name, setName] = useState("");
@@ -31,19 +31,33 @@ function Nav({ setLoading, setSave, save }) {
     }
   }
 
-  console.log(save);
+  async function saveChanges() {
+    try {
+      const path = await dialog.save({
+        title: "Save changes",
+        defaultPath: name,
+      });
+
+      const saved = await invoke("save", {
+        save: JSON.stringify(save),
+        path: path,
+      });
+
+      console.log(saved);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <nav className="nav">
-      <button
-        disabled={save == null ? false : true}
-        id="openSave"
-        onClick={readFile}
-      >
+      <button id="openSave" onClick={readFile}>
         Open
       </button>
       <span>{name}</span>
-      <button>Save</button>
+      <button disabled={save == null ? true : false} onClick={saveChanges}>
+        Save
+      </button>
     </nav>
   );
 }
