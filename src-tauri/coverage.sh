48982761d98d2032269c37368c126852ea31ac39
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-08-16 19:38:48 -0300

    Exclude release files from coverage test

--- src-tauri/coverage.sh
+++ src-tauri/coverage.sh
@@ -1,1 +1,1 @@
-cargo tarpaulin --target-dir target/tarpaulin/artifacts --skip-clean  --exclude-files src/main.rs "target/debug/**/*"
+cargo tarpaulin --target-dir target/tarpaulin/artifacts --skip-clean  --exclude-files src/main.rs "target/debug/**/*" "target/release/**/*"
