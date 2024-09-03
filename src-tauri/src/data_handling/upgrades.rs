ee6b07f3898e392b39d2e20190c552dd0d39a63f
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-09-03 00:08:45 -0300

    Add effect names to Upgrade and refactor parse_upgrades()

--- src-tauri/src/data_handling/upgrades.rs
+++ src-tauri/src/data_handling/upgrades.rs
@@ -4,8 +4,7 @@ use super::{enums::{UpgradeType, Error},
             file::FileData};
 use std::{fs::File,
           io::BufReader,
-          collections::HashMap,
-          path::PathBuf};
+          collections::HashMap};
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct UpgradeInfo {
     pub name: String,
@@ -21,7 +20,7 @@ pub struct Upgrade {
     pub source: u32,
     pub upgrade_type: UpgradeType,
     pub shape: String,
-    pub effects: [u32; 6],
+    pub effects: Vec<(u32, String)>,
     pub info: UpgradeInfo,
 }
 
@@ -31,7 +30,7 @@ pub fn parse_upgrades(file_data: &FileData) -> HashMap<UpgradeType, Vec<Upgrade>
     let json_file =  File::open(file_path).map_err(Error::IoError).unwrap();
     let reader = BufReader::new(json_file);
     let upgrades_json: Value = serde_json::from_reader(reader).unwrap();
-    
+
     let (start, end) = file_data.offsets.upgrades;
 
     for i in (start .. end).step_by(40) {
@@ -42,24 +41,47 @@ pub fn parse_upgrades(file_data: &FileData) -> HashMap<UpgradeType, Vec<Upgrade>
 
         let upgrade_type = UpgradeType::try_from(file_data.bytes[i+8]).unwrap();
 
-        let mut effects = [0; 6];
-        effects[0] = u32::from_le_bytes([file_data.bytes[i + 16], file_data.bytes[i + 17], file_data.bytes[i + 18], file_data.bytes[i + 19]]);
+        let mut effects: Vec<(u32, String)> = Vec::with_capacity(6);
+        let mut effects_ids = [0; 6];
+        effects_ids[0] = u32::from_le_bytes([file_data.bytes[i + 16], file_data.bytes[i + 17], file_data.bytes[i + 18], file_data.bytes[i + 19]]);
 
-        effects[1] = u32::from_le_bytes([file_data.bytes[i + 20], file_data.bytes[i + 21], file_data.bytes[i + 22], file_data.bytes[i + 23]]);
+        effects_ids[1] = u32::from_le_bytes([file_data.bytes[i + 20], file_data.bytes[i + 21], file_data.bytes[i + 22], file_data.bytes[i + 23]]);
 
-        effects[2] = u32::from_le_bytes([file_data.bytes[i + 24], file_data.bytes[i + 25], file_data.bytes[i + 26], file_data.bytes[i + 27]]);
+        effects_ids[2] = u32::from_le_bytes([file_data.bytes[i + 24], file_data.bytes[i + 25], file_data.bytes[i + 26], file_data.bytes[i + 27]]);
 
-        effects[3] = u32::from_le_bytes([file_data.bytes[i + 28], file_data.bytes[i + 29], file_data.bytes[i + 30], file_data.bytes[i + 31]]);
+        effects_ids[3] = u32::from_le_bytes([file_data.bytes[i + 28], file_data.bytes[i + 29], file_data.bytes[i + 30], file_data.bytes[i + 31]]);
 
-        effects[4] = u32::from_le_bytes([file_data.bytes[i + 32], file_data.bytes[i + 33], file_data.bytes[i + 34], file_data.bytes[i + 35]]);
+        effects_ids[4] = u32::from_le_bytes([file_data.bytes[i + 32], file_data.bytes[i + 33], file_data.bytes[i + 34], file_data.bytes[i + 35]]);
 
-        effects[5] = u32::from_le_bytes([file_data.bytes[i + 36], file_data.bytes[i + 37], file_data.bytes[i + 38], file_data.bytes[i + 39]]);
+        effects_ids[5] = u32::from_le_bytes([file_data.bytes[i + 36], file_data.bytes[i + 37], file_data.bytes[i + 38], file_data.bytes[i + 39]]);
 
 
-        let info = match get_info_upgrade(effects[0], upgrade_type, &upgrades_json) {
-            Ok(inf) => inf,
-            Err(_) => continue,
+        let json_effects: &Value = match upgrade_type {
+            UpgradeType::Gem => &upgrades_json["gemEffects"],
+            UpgradeType::Rune => &upgrades_json["runeEffects"],
         };
+
+        let mut info = UpgradeInfo {
+            name: String::from(""),
+            effect: String::from("No Effect"),
+            rating: 95,
+            level: 0,
+            note: String::from(""),
+        };
+
+        for e in 0 .. 6 {
+            let json_effect = &json_effects[&effects_ids[e].to_string()];
+            let effect_info: UpgradeInfo = match serde_json::from_value(json_effect.clone()) {
+                Ok(inf) => inf,
+                Err(_) => continue,
+            };
+            effects.push((effects_ids[e], effect_info.effect.clone()));
+            if e == 0 {
+                info = effect_info;
+            }
+
+        }
+
 
         let shape = match get_shape(file_data.bytes[i+12], upgrade_type) {
             Ok(sha) => sha,
@@ -78,19 +100,6 @@ pub fn parse_upgrades(file_data: &FileData) -> HashMap<UpgradeType, Vec<Upgrade>
         category.push(upgrade);
     };
     upgrades
-}
-
-pub fn get_info_upgrade(first_effect: u32, upgrade_type: UpgradeType, upgrades: &Value) -> Result<UpgradeInfo, Error> {
-    let upgrades = match upgrade_type {
-        UpgradeType::Gem => &upgrades["gemEffects"],
-        UpgradeType::Rune => &upgrades["runeEffects"],
-    };
-    
-    let upgrade = &upgrades[&first_effect.to_string()];
-    match serde_json::from_value(upgrade.clone()) {
-        Ok(info) => Ok(info),
-        Err(_) => Err(Error::CustomError("ERROR: Failed to find info for the upgrade."))
-    }
 }
 
 pub fn get_shape(shape: u8, upgrade_type: UpgradeType) -> Result<String, Error> {
@@ -135,12 +144,12 @@ mod tests {
         assert_eq!(gems[0].id, u32::from_le_bytes([0x41, 0x00, 0x80, 0xC0]));
         assert_eq!(gems[0].source, u32::from_le_bytes([0x26, 0x60, 0x01, 0x80]));
         assert_eq!(gems[0].upgrade_type, UpgradeType::Gem);
-        assert_eq!(gems[0].effects, [0x440c,
-                                     0x440c,
-                                     0x440c,
-                                     0x440c,
-                                     0x440c,
-                                     0x440c]);
+        assert_eq!(gems[0].effects, vec![(0x440c, String::from("Adds physical ATK (+45)")),
+                                         (0x440c, String::from("Adds physical ATK (+45)")),
+                                         (0x440c, String::from("Adds physical ATK (+45)")),
+                                         (0x440c, String::from("Adds physical ATK (+45)")),
+                                         (0x440c, String::from("Adds physical ATK (+45)")),
+                                         (0x440c, String::from("Adds physical ATK (+45)"))]);
         assert_eq!(gems[0].shape, String::from("Droplet"));
         let info = gems[0].info.clone();
         assert_eq!(info.name, String::from("Abyssal Blood Gem"));
@@ -153,12 +162,12 @@ mod tests {
         assert_eq!(runes[0].id, u32::from_le_bytes([0x42, 0x00, 0x80, 0xC0]));
         assert_eq!(runes[0].source, u32::from_le_bytes([0xBF, 0x92, 0x01, 0x80]));
         assert_eq!(runes[0].upgrade_type, UpgradeType::Rune);
-        assert_eq!(runes[0].effects, [0x115582,
-                                      0xffffffff,
-                                      0xffffffff,
-                                      0xffffffff,
-                                      0xffffffff,
-                                      0xffffffff]);
+        assert_eq!(runes[0].effects, vec![(0x115582, String::from("Max QS bullets held UP +3")),
+                                          (0xffffffff, String::from("No Effect")),
+                                          (0xffffffff, String::from("No Effect")),
+                                          (0xffffffff, String::from("No Effect")),
+                                          (0xffffffff, String::from("No Effect")),
+                                          (0xffffffff, String::from("No Effect"))]);
         assert_eq!(runes[0].shape, String::from("-"));
         let info = runes[0].info.clone();
         assert_eq!(info.name, String::from("Formless Oedon"));
@@ -178,12 +187,12 @@ mod tests {
         assert_eq!(gems[0].id, u32::from_le_bytes([0x67, 0x00, 0x80, 0xC0]));
         assert_eq!(gems[0].source, u32::from_le_bytes([0xF0, 0x49, 0x02, 0x80]));
         assert_eq!(gems[0].upgrade_type, UpgradeType::Gem);
-        assert_eq!(gems[0].effects, [0x2FB3BC,
-                                     0x2E7754,
-                                     0xffffffff,
-                                     0xffffffff,
-                                     0xffffffff,
-                                     0xffffffff]);
+        assert_eq!(gems[0].effects, vec![(0x2FB3BC, String::from("Physical ATK UP (+2.7%)")),
+                                         (0x2E7754, String::from("Boosts rally potential (+1.8%)")),
+                                         (0xffffffff, String::from("No Effect")),
+                                         (0xffffffff, String::from("No Effect")),
+                                         (0xffffffff, String::from("No Effect")),
+                                         (0xffffffff, String::from("No Effect"))]);
         assert_eq!(gems[0].shape, String::from("Droplet"));
         let info = gems[0].info.clone();
         assert_eq!(info.name, String::from("Tempering Blood Gemstone (1)"));
