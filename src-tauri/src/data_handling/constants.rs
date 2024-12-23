c938d54574e13a5b285764825bece445ed37128b
Noxde <mateo.tesorieri@hotmail.com>
2024-12-23 00:30:26 -0300

    Added new constats to the inventory counters

--- src-tauri/src/data_handling/constants.rs
+++ src-tauri/src/data_handling/constants.rs
@@ -7,6 +7,18 @@ pub const USERNAME_TO_KEY_INV_OFFSET: usize = 32201;
 //Distance between the username and the beginning of the storage inventory
 pub const INV_TO_STORAGE_OFFSET: usize = 34268;
 
+// Distance between the username and the first inventory counter
+pub const USERNAME_TO_FIRST_INVENTORY_COUNTER: usize = 453;
+
+// Distance between the username and the second inventory counter
+pub const USERNAME_TO_SECOND_INVENTORY_COUNTER: usize = 34257;
+
+// Distance between the username and the first inventory counter
+pub const USERNAME_TO_FIRST_STORAGE_COUNTER: usize = 34737;
+
+// Distance between the username and the second inventory counter
+pub const USERNAME_TO_SECOND_STORAGE_COUNTER: usize = 68541;
+
 //Distance from the start of the save to the first gem/rune
 pub const START_TO_UPGRADE: usize = 84;
 
