7c3ed4897529e16b55d245b5c1c43a585944a0b0
Noxde <mateo.tesorieri@hotmail.com>
2025-01-21 21:50:16 -0300

    New component to choose gems to equip/unequip

---
+++ src/components/ChangeGemScreen.jsx
@@ -0,0 +1,124 @@
+import { useContext, useState } from "react";
+import { SaveContext } from "../context/context";
+import Item from "./Item";
+import { invoke } from "@tauri-apps/api/tauri";
+
+function ChangeGemScreen({
+  article,
+  setSelected,
+  setScreen,
+  slotIndex,
+  isStorage,
+}) {
+  const { save, setSave } = useContext(SaveContext);
+  const gems = [
+    ...(save.inventory.upgrades.Gem ?? []),
+    ...(save.storage.upgrades.Gem ?? []),
+  ];
+
+  const [hoverIndex, setHoverIndex] = useState(null);
+  const [selectedGem, setSelectedGem] = useState(null);
+  const dummy = {
+    number: -1,
+    id: 0,
+    source: 0,
+    upgrade_type: "Gem",
+    shape: "",
+    effects: [
+      [4294967295, "No Effect"],
+      [4294967295, "No Effect"],
+      [4294967295, "No Effect"],
+      [4294967295, "No Effect"],
+      [4294967295, "No Effect"],
+      [4294967295, "No Effect"],
+    ],
+    info: {
+      name: "Unequip gem",
+      effect: "",
+      rating: 0,
+      level: 0,
+      note: "",
+    },
+    index: 2,
+  };
+  gems.unshift(dummy);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          background: "#000000cc",
+          width: "100%",
+          height: "calc(100vh - 111px)",
+          zIndex: 999,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            position: "relative",
+            width: "534px",
+            minHeight: "400px",
+            maxHeight: "calc(100% - 149px)",
+            background: "black",
+            overflowX: "hidden",
+            overflowY: "scroll",
+          }}
+        >
+          <div
+            id="hoverReplacement"
+            style={{
+              display: hoverIndex == null ? "none" : "block",
+              top: `${hoverIndex * 91}px`,
+            }}
+          ></div>
+          {gems.map((x, i) => (
+            <Item
+              item={x}
+              index={i + 1}
+              isSmall={false}
+              onClick={() => {
+                setHoverIndex(i);
+                setSelectedGem(x);
+              }}
+            />
+          ))}
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <button
+            style={{ marginRight: "1rem" }}
+            onClick={() => setScreen(false)}
+          >
+            Cancel
+          </button>
+          <button
+            onClick={async () => {
+              if (selectedGem.number === -1) {
+                const edited = await invoke("unequip_gem", {
+                  articleType: article.article_type,
+                  articleIndex: article.index,
+                  slotIndex: slotIndex,
+                  isStorage: isStorage,
+                });
+
+                setSave(edited);
+                article.slots[slotIndex].gem = null;
+                setSelected(null);
+
+                setScreen(false);
+              }
+            }}
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ChangeGemScreen;
