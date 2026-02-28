57b29ac408b4c870cae6dbc1af78eb5884a6a1cd
Noxde <mateo.tesorieri@hotmail.com>
2026-02-28 16:54:21 -0300

    Added tooltip, doll's lullaby and added blood addled

--- src/pages/flags/Flag.jsx
+++ src/pages/flags/Flag.jsx
@@ -1,6 +1,17 @@
 import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useRef, useState } from "react";
 
-function Flag({ label, offset, values }) {
+function Flag({ label, offset, values, info }) {
+  const tipRef = useRef();
+  const [top, setTop] = useState(0);
+  const [showTip, setShowTip] = useState(false);
+
+  useEffect(() => {
+    if (tipRef.current) {
+      setTop(tipRef.current.getBoundingClientRect().height);
+    }
+  }, []);
+
   async function setFlag() {
     console.log(values);
     for (let i = 0; i < values.length; i++) {
@@ -16,10 +27,40 @@ function Flag({ label, offset, values }) {
       style={{
         display: "flex",
         justifyContent: "space-between",
+        alignItems: "center",
         width: "100%",
+        position: "relative",
       }}
     >
-      <label>{label}</label>
+      {/* Tooltip info */}
+      <div
+        className="tooltip"
+        style={{
+          opacity: showTip ? 1 : 0,
+          top: `-${top + 5}px`,
+        }}
+        ref={tipRef}
+      >
+        {info}
+      </div>
+
+      {/* Tooltip hover and label */}
+      <div
+        style={{
+          position: "relative",
+          paddingLeft: 25,
+        }}
+      >
+        <div
+          className="tooltip-hover"
+          onMouseEnter={() => setShowTip(true)}
+          onMouseLeave={() => setShowTip(false)}
+        >
+          ?
+        </div>
+        <label>{label}</label>
+      </div>
+
       <button
         style={{
           padding: "0rem 1rem",
--- src/pages/flags/Flags.jsx
+++ src/pages/flags/Flags.jsx
@@ -1,4 +1,5 @@
 import Flag from "./Flag";
+import "./flags.css";
 
 function Flags() {
   return (
@@ -25,14 +26,32 @@ function Flags() {
           gap: "2rem",
 
           overflowY: "auto",
-          overflowX: "hidden",
+          overflowX: "visible",
           height: "100%",
           width: "100%",
           paddingBottom: "2px",
         }}
       >
-        <Flag label="Restore Maria's dialogue" offset={1083} values={[0, 8]} />
-        <Flag label="Enable Doll's lullaby" offset={6689} values={[1, 8]} />
+        <Flag
+          label="Restore Maria's dialogue"
+          offset={1083}
+          values={[0, 8]}
+          info={"Restores some dialogue before fighting Maria."}
+        />
+        <Flag
+          label="Enable Doll's lullaby"
+          offset={6689}
+          values={[8, 1]}
+          info={
+            "Enables the doll's lullaby from version 1.0, the doll needs to be sleeping to trigger when enabled."
+          }
+        />
+        <Flag
+          label="Enable Blood addled"
+          offset={4127}
+          values={[162]}
+          info={"Enables pvp when looking for coop."}
+        />
       </div>
     </div>
   );
